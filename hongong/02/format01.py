6b9ffde138a94c2fa5302c1d51417df639a52935
haeun Noh <okhagrace06@naver.com>
2023-02-04 17:12:01 +0900

    [혼공단9기] 2주차 챕터02

---
+++ hongong/02/format01.py
@@ -0,0 +1,9 @@
+# format() 함수로 숫자를 문자열로 변환하기
+format_a = "{}만원".format(5000)
+format_b = "파이썬 열공하여 첫 연봉 {}만 원 만들기 ".format(5000)
+format_c = "{} {} {}".format(3000, "문자열", True)
+
+# 출력
+print(format_a)
+print(format_b)
+print(format_c)
---
+++ hongong/02/inch_to_cm.py
@@ -0,0 +1,9 @@
+# 입력
+raw_input = input("inch 단위의 숫자를 입력해주세요: ")
+
+# 처리
+inch = int(raw_input)
+cm = inch * 2.54
+
+# 출력
+print(inch, "inch는 cm 단위로 ", cm,"cm입니다.")
