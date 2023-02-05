c728237d62c0b41091a37f66c719fba8da9934dc
haeun Noh <okhagrace06@naver.com>
2023-02-05 20:31:52 +0900

    [혼공단9기] 가변 매개변수 에러 테스트

--- hongong/05/param_basic.py
+++ hongong/05/param_basic.py
@@ -1,5 +1,5 @@
-def print_n_times(value, n):
+def print_n_times(value, n): # 매개변수를 두 개 지정했는데
     for i in range(n):
         print(value)
 
-print_n_times("안녕하세요", 5)
+print_n_times("안녕하세요", 10, 20) # 하나만 넣었음
